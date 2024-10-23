cb1dc6dcdac5f458ad679fcbab73e69a54d6e2dd
Tomasz Jarosik <tjarosik@proton.me>
2024-10-23 22:35:22 +0100

    Add fugaci_test package for end-to-end system tests

---
+++ pkg/fugaci_test/provider_e2e_helpers_test.go
@@ -0,0 +1,151 @@
+package fugaci_test
+
+import (
+	"bytes"
+	"context"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/remotecommand"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func createPodFromYAML(clientset *kubernetes.Clientset, fileName string) (*v1.Pod, error) {
+	podYAML, err := os.ReadFile(filepath.Join("testdata", fileName))
+	if err != nil {
+		return nil, fmt.Errorf("failed to read YAML file: %w", err)
+	}
+
+	// Use the Kubernetes scheme to decode the YAML
+	decoder := serializer.NewCodecFactory(scheme.Scheme).UniversalDeserializer()
+	obj, _, err := decoder.Decode(podYAML, nil, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode YAML: %w", err)
+	}
+
+	// Assert that the object is a Pod
+	pod, ok := obj.(*v1.Pod)
+	if !ok {
+		return nil, fmt.Errorf("decoded object is not a Pod")
+	}
+
+	pod.Name = generateRandomName(pod.Name)
+	pod.Namespace = testNamespace
+	return clientset.CoreV1().Pods(testNamespace).Create(context.TODO(), pod, metav1.CreateOptions{})
+}
+
+func deletePod(clientset *kubernetes.Clientset, podName string) {
+	clientset.CoreV1().Pods(testNamespace).Delete(context.TODO(), podName, metav1.DeleteOptions{})
+}
+
+func waitForPodPhase(clientset *kubernetes.Clientset, namespace, podName string, desiredPhase v1.PodPhase, timeout time.Duration) error {
+	return wait.PollUntilContextTimeout(context.Background(), 1*time.Second, timeout, true, func(ctx context.Context) (done bool, err error) {
+		pod, err := clientset.CoreV1().Pods(namespace).Get(context.TODO(), podName, metav1.GetOptions{})
+		if err != nil {
+			return false, err
+		}
+		return pod.Status.Phase == desiredPhase, nil
+	})
+}
+
+func waitForPodPhaseRunning(clientset *kubernetes.Clientset, namespace, podName string, timeout time.Duration) error {
+	return waitForPodPhase(clientset, namespace, podName, v1.PodRunning, timeout)
+}
+
+func waitForPodPhaseFailed(clientset *kubernetes.Clientset, namespace, podName string, timeout time.Duration) error {
+	return waitForPodPhase(clientset, namespace, podName, v1.PodFailed, timeout)
+}
+
+func waitForPodPhaseSucceeded(clientset *kubernetes.Clientset, namespace, podName string, timeout time.Duration) error {
+	return waitForPodPhase(clientset, namespace, podName, v1.PodSucceeded, timeout)
+}
+
+func waitForPodConditionReady(clientset *kubernetes.Clientset, namespace, podName string, timeout time.Duration) error {
+	return wait.PollUntilContextTimeout(context.Background(), 1*time.Second, timeout, true, func(ctx context.Context) (bool, error) {
+		pod, err := clientset.CoreV1().Pods(namespace).Get(context.TODO(), podName, metav1.GetOptions{})
+		if err != nil {
+			return false, err
+		}
+		return isPodReady(pod), nil
+	})
+}
+
+// Helper function for common assertions
+func assertPodHasLabel(t *testing.T, pod *v1.Pod, key, expectedValue string) {
+	value, exists := pod.Labels[key]
+	assert.True(t, exists, "Pod %s should have label %s", pod.Name, key)
+	assert.Equal(t, expectedValue, value, "Pod %s label %s should be %s", pod.Name, key, expectedValue)
+}
+
+func getContainerLogs(t *testing.T, clientset *kubernetes.Clientset, namespace, podName, containerName string) string {
+	t.Helper()
+	podLogOpts := v1.PodLogOptions{Container: containerName}
+	req := clientset.CoreV1().Pods(namespace).GetLogs(podName, &podLogOpts)
+	podLogs, err := req.Stream(context.TODO())
+	require.NoError(t, err, "error opening log stream for pod %s", podName)
+	defer podLogs.Close()
+
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, podLogs)
+	require.NoError(t, err, "error copying logs")
+	return buf.String()
+}
+
+func execCommandInPod(t *testing.T, clientset *kubernetes.Clientset, config *rest.Config, namespace, podName, containerName string, command []string) (string, string) {
+	t.Helper()
+	req := clientset.CoreV1().RESTClient().
+		Post().
+		Namespace(namespace).
+		Resource("pods").
+		Name(podName).
+		SubResource("exec").
+		VersionedParams(&v1.PodExecOptions{
+			Container: containerName,
+			Command:   command,
+			Stdin:     false,
+			Stdout:    true,
+			Stderr:    true,
+			TTY:       false,
+		}, scheme.ParameterCodec)
+
+	exec, err := remotecommand.NewSPDYExecutor(config, "POST", req.URL())
+	require.NoError(t, err, "error creating executor")
+	var stdout, stderr bytes.Buffer
+
+	err = exec.Stream(remotecommand.StreamOptions{
+		Stdout: &stdout,
+		Stderr: &stderr,
+	})
+	require.NoError(t, err, "error executing command: %v", command)
+	return stdout.String(), stderr.String()
+}
+
+func generateRandomName(baseName string) string {
+	b := make([]byte, 4) // 4 bytes will give us 8 hex characters
+	if _, err := rand.Read(b); err != nil {
+		panic("failed to generate random bytes")
+	}
+	return fmt.Sprintf("%s-%s", baseName, hex.EncodeToString(b))
+}
+
+func isPodReady(pod *v1.Pod) bool {
+	for _, condition := range pod.Status.Conditions {
+		if condition.Type == v1.PodReady {
+			return condition.Status == v1.ConditionTrue
+		}
+	}
+	return false
+}
---
+++ pkg/fugaci_test/provider_e2e_test.go
@@ -0,0 +1,209 @@
+package fugaci_test
+
+/*
+	To run these tests you need to have full setup with at least real Mac node connected to Kubernetes cluster
+	You also need fugaci-ssh-secret to be defined.
+	You might need to modify VM image reference too.
+*/
+
+import (
+	"context"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"k8s.io/client-go/rest"
+	"testing"
+	"time"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+const testNamespace = "jenkins"
+
+type PodTestCase struct {
+	name       string
+	podFiles   []string
+	timeout    time.Duration
+	postCreate func(clientset *kubernetes.Clientset, namespace, podName string, timeout time.Duration) error
+	assertions func(t *testing.T, clientset *kubernetes.Clientset, config *rest.Config, pods []*v1.Pod)
+}
+
+func TestProviderE2E(t *testing.T) {
+	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
+	if err != nil {
+		t.Fatalf("Failed to build kubeconfig: %v", err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		t.Fatalf("Failed to create Kubernetes client: %v", err)
+	}
+
+	// TODO(tjarosik):
+	// Create the namespace for testing
+	//if err := createNamespace(clientset); err != nil {
+	//	t.Fatalf("Failed to create test namespace: %v", err)
+	//}
+	//defer deleteNamespace(clientset)
+
+	// Define test cases
+	tests := []PodTestCase{
+		{
+			name:       "TestBasicPod_MustBeRunning",
+			podFiles:   []string{"pod1-basic-running.yaml"},
+			timeout:    15 * time.Second,
+			postCreate: waitForPodPhaseRunning,
+			assertions: func(t *testing.T, clientset *kubernetes.Clientset, config *rest.Config, pods []*v1.Pod) {
+				p := pods[0]
+				require.Equal(t, v1.PodRunning, p.Status.Phase)
+			},
+		},
+		{
+			name:       "TestPodWithInvalidImage_MustBeInFailedPhase",
+			podFiles:   []string{"pod2-invalid-image.yaml"},
+			timeout:    15 * time.Second,
+			postCreate: waitForPodPhaseFailed,
+			assertions: func(t *testing.T, clientset *kubernetes.Clientset, config *rest.Config, pods []*v1.Pod) {
+				p := pods[0]
+				logs := getContainerLogs(t, clientset, testNamespace, p.Name, "curie3")
+				assert.Contains(t, logs, "err=\"pull image: GET https://ghcr.com/v2/invalid/image/manifests/123: unexpected status code 404 Not Found")
+
+				containerStatus := p.Status.ContainerStatuses[0]
+				assert.False(t, containerStatus.Ready)
+				assert.Equal(t, "unable to pull image", containerStatus.State.Terminated.Reason)
+			},
+		},
+		{
+			name:       "TestBasicPod_MustReturnLogs",
+			podFiles:   []string{"pod1-basic-running.yaml"},
+			timeout:    20 * time.Second,
+			postCreate: waitForPodConditionReady,
+			assertions: func(t *testing.T, clientset *kubernetes.Clientset, config *rest.Config, pods []*v1.Pod) {
+				p := pods[0]
+				logs := getContainerLogs(t, clientset, testNamespace, p.Name, "curie3")
+				assert.Contains(t, logs, "spec.image=ghcr.io/macvmio/macos-sonoma:14.5-agent-v1.6")
+				assert.Contains(t, logs, "action=pulling")
+			},
+		},
+		{
+			name:       "TestBasicPod_MustBeAbleToExecuteCommand",
+			podFiles:   []string{"pod1-basic-running.yaml"},
+			postCreate: waitForPodConditionReady,
+			assertions: func(t *testing.T, clientset *kubernetes.Clientset, config *rest.Config, pods []*v1.Pod) {
+				p := pods[0]
+				command := []string{"sw_vers"}
+				stdout, stderr := execCommandInPod(t, clientset, config, testNamespace, p.Name, "curie3", command)
+				assert.Contains(t, stdout, "macOS")
+				assert.Contains(t, stdout, "14.5")
+				assert.Empty(t, stderr, "Stderr should be empty")
+			},
+		},
+		{
+			name:       "TestEphemeralPod_ShortLived",
+			podFiles:   []string{"pod3-shortlived.yaml"},
+			postCreate: waitForPodPhaseSucceeded,
+			assertions: func(t *testing.T, clientset *kubernetes.Clientset, config *rest.Config, pods []*v1.Pod) {
+				p := pods[0]
+				logs := getContainerLogs(t, clientset, testNamespace, p.Name, "curie3")
+
+				assert.Contains(t, logs, "spec.image=ghcr.io/macvmio/macos-sonoma:14.5-agent-v1.6")
+				assert.Contains(t, logs, "action=pulling")
+				assert.Contains(t, logs, "imageID=sha256:08bbe35549962a2aef9e79631f93c43a245aa662674cb88298be518aabbaed32")
+				assert.Contains(t, logs, "state=created")
+				assert.Contains(t, logs, "state=SSHReady")
+				assert.Contains(t, logs, "action=stop")
+				assert.Contains(t, logs, "container_command=\"[sleep 1]\"")
+				assert.Contains(t, logs, "container_exitcode=0")
+				//assert.NotContains(t, logs, "process still did not exit")
+			},
+		},
+		{
+			name:       "TestPodWithCustomEnvVars",
+			podFiles:   []string{"pod4-custom-env-vars.yaml"},
+			postCreate: waitForPodConditionReady,
+			assertions: func(t *testing.T, clientset *kubernetes.Clientset, config *rest.Config, pods []*v1.Pod) {
+				p := pods[0]
+				command := []string{"env"}
+				stdout, stderr := execCommandInPod(t, clientset, config, testNamespace, p.Name, "curie3", command)
+				assert.Contains(t, stdout, "KUBERNETES_POD_NAME=testpod4")
+				assert.Contains(t, stdout, "FUGACI_TESTKEY=some-test-value")
+				assert.Contains(t, stdout, fmt.Sprintf("KUBERNETES_NODE_NAME=%v", p.Status.NominatedNodeName))
+				assert.NotContains(t, stdout, "FUGACI_SSH_PASSWORD")
+				assert.NotContains(t, stdout, "FUGACI_SSH_USERNAME")
+				assert.Empty(t, stderr, "Stderr should be empty")
+			},
+		},
+		// TODO: Test all 3 pulling strategies
+		// TODO: Test invalid image
+		// Add more test cases here
+	}
+
+	for _, tc := range tests {
+		tc2 := tc // capture range variable
+		t.Run(tc2.name, func(t *testing.T) {
+			runPodTest(t, clientset, config, tc2)
+		})
+	}
+}
+
+func createNamespace(clientset *kubernetes.Clientset) error {
+	ns := &v1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: testNamespace,
+		},
+	}
+	_, err := clientset.CoreV1().Namespaces().Create(context.TODO(), ns, metav1.CreateOptions{})
+	return err
+}
+
+func deleteNamespace(clientset *kubernetes.Clientset) {
+	clientset.CoreV1().Namespaces().Delete(context.TODO(), testNamespace, metav1.DeleteOptions{})
+}
+
+func runPodTest(t *testing.T, clientset *kubernetes.Clientset, config *rest.Config, tc PodTestCase) {
+	var pods []*v1.Pod
+	var podNames []string
+
+	// Create pods from YAML files
+	for _, podFile := range tc.podFiles {
+		pod, err := createPodFromYAML(clientset, podFile)
+		require.NoError(t, err)
+		pods = append(pods, pod)
+		podNames = append(podNames, pod.Name)
+	}
+
+	defer func() {
+		for _, podName := range podNames {
+			deletePod(clientset, podName)
+		}
+	}()
+
+	valueOrDefault := func(a, b time.Duration) time.Duration {
+		if 0 != a {
+			return a
+		}
+		return b
+	}
+
+	for _, pod := range pods {
+		// TODO: Make it configurable per test? e.g. waitForRead, waitForFinished, waitForRunning
+		if err := tc.postCreate(clientset, pod.Namespace, pod.Name, valueOrDefault(tc.timeout, 30*time.Second)); err != nil {
+			t.Fatalf("Pod %s did not reach Ready state: %v", pod.Name, err)
+		}
+	}
+
+	for i, pod := range pods {
+		updatedPod, err := clientset.CoreV1().Pods(testNamespace).Get(context.TODO(), pod.Name, metav1.GetOptions{})
+		if err != nil {
+			t.Fatalf("Failed to get updated pod %s: %v", pod.Name, err)
+		}
+		pods[i] = updatedPod
+	}
+
+	if tc.assertions != nil {
+		tc.assertions(t, clientset, config, pods)
+	}
+}
